1ce790b85d01d20ebc6399f8fd9fb32e61fbe989
elsigodolja <elsi@knockrentals.com>
2021-04-07 14:36:05 -0400

    fix: comment

--- sub-package/main.js
+++ sub-package/main.js
@@ -4,7 +4,7 @@ function add(x, y) {
     return res;
 }
 
-// this function multiplies two numbers
+// this function multiplies two numbers,
 function multiply(x, y) {
     let res = x * y;
     return res;
